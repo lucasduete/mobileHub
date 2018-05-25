d719f360a918e58e631fe0e03e3c2774b51f8917
Lucas Duete <lm.duete@gmail.com>
2018-05-25 03:17:56 -0300

    Implementando Tela de Listagem de Estrelas.

--- app/src/main/java/lucasduete/github/io/mobilehub/manager/MenuManage.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/manager/MenuManage.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.MenuItem;
 
+import lucasduete.github.io.mobilehub.ListRepositoriesActivity;
+import lucasduete.github.io.mobilehub.ListStarsActivity;
 import lucasduete.github.io.mobilehub.MainActivity;
 import lucasduete.github.io.mobilehub.ProfileActivity;
 import lucasduete.github.io.mobilehub.R;
@@ -22,6 +24,12 @@ public class MenuManage {
                 break;
             case R.id.nav_perfil:
                 intent = new Intent(context, ProfileActivity.class);
+                break;
+            case R.id.nav_meusRepositorios:
+                intent = new Intent(context, ListRepositoriesActivity.class);
+                break;
+            case R.id.nav_minhasEstrelas:
+                intent = new Intent(context, ListStarsActivity.class);
                 break;
             case R.id.nav_buscar:
                 intent = new Intent(context, SearchActivity.class);
