060e7346e66636214cbaebea8f8200399bae7dfb
Lucas Duete <lm.duete@gmail.com>
2018-06-18 16:25:09 -0300

    Implementando Handle do LoginActivity

--- app/src/main/java/lucasduete/github/io/mobilehub/LoginActivity.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/LoginActivity.java
@@ -1,13 +1,46 @@
 package lucasduete.github.io.mobilehub;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import lucasduete.github.io.mobilehub.utils.ConstManager;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static MyHandle loginHandle;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.context = this;
+    }
+
+    private class MyHandle extends Handler {
+        public MyHandle() {
+
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            Log.d(ConstManager.TAG, "Chegou no Handle");
+            Boolean result = (Boolean) msg.obj;
+
+            Log.d(ConstManager.TAG, String.format("O valor foi: %s", result.toString()));
+
+            if (result) {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            } else
+                Toast.makeText(context, "Deu ruim no Login", Toast.LENGTH_SHORT).show();
+        }
     }
 }
