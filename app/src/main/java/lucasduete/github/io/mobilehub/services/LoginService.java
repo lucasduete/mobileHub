5067f6a0532b83ef94a52ced6fc245c40b0f4ed0
Lucas Duete <lm.duete@gmail.com>
2018-06-18 20:14:54 -0300

    Refatorando loginService para modularização e implemetação dos três modos: Basic, Oauth e Authorizate

--- app/src/main/java/lucasduete/github/io/mobilehub/services/LoginService.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/services/LoginService.java
@@ -23,6 +23,11 @@ import okhttp3.Response;
 
 public class LoginService extends Service {
 
+    public static final int MODE_BASIC = 1;
+    public static final int MODE_OAUTH = 2;
+    public static final int MODE_AUTHORIZATE = 3;
+    public static final int MODE_ERROR = 0;
+
     public LoginService() {
 
     }
@@ -40,34 +45,25 @@ public class LoginService extends Service {
             Log.d(ConstManager.TAG, "Chegou no Service de Login");
 
             OkHttpClient client = new OkHttpClient();
-
             MediaType mediaType = MediaType.parse("application/json; charset=utf8");
             RequestBody requestBody = RequestBody.create(mediaType, "json");
 
-            Request request = new Request.Builder()
-                    .url(String.format("%s/%s", ConstManager.URL_BASE, "login/basic"))
-                    .post(requestBody)
-                    .build();
+            int mode = intent.getIntExtra("mode", 0);
 
-            JSONObject jsonObject = null;
 
-            try {
-                Response response = client.newCall(request).execute();
-                jsonObject = new JSONObject(response.body().string());
-            } catch (IOException ex) {
-                Log.d(ConstManager.TAG, "\n\nDeu na conexão");
-                ex.printStackTrace();
-                failLogin();
-            } catch (JSONException ex) {
-                Log.d(ConstManager.TAG, "\n\nDeu ruim no JSON");
-                ex.printStackTrace();
-                failLogin();
-            }
-
-            if (jsonObject != null) {
-                Message msg = new Message();
-                msg.obj = true;
-                LoginActivity.loginHandle.sendMessage(msg);
+            switch (mode) {
+                case MODE_BASIC:
+                    basicMode(client, requestBody);
+                    break;
+                case MODE_OAUTH:
+                    oauthMode(client);
+                    break;
+                case MODE_AUTHORIZATE:
+                    authorizationMode(client, requestBody);
+                    break;
+                default:
+                    failLogin();
+                    break;
             }
 
         }).start();
@@ -75,8 +71,92 @@ public class LoginService extends Service {
         return Service.START_NOT_STICKY;
     }
 
+    private void basicMode(OkHttpClient client, RequestBody requestBody) {
+        Request request = new Request.Builder()
+                .url(String.format("%s/%s", ConstManager.URL_BASE, "login/basic"))
+                .post(requestBody)
+                .build();
+
+        JSONObject jsonObject = null;
+
+        try {
+            Response response = client.newCall(request).execute();
+            jsonObject = new JSONObject(response.body().string());
+        } catch (IOException ex) {
+            Log.d(ConstManager.TAG, "\n\nDeu na conexão");
+            ex.printStackTrace();
+            failLogin();
+        } catch (JSONException ex) {
+            Log.d(ConstManager.TAG, "\n\nDeu ruim no JSON");
+            ex.printStackTrace();
+            failLogin();
+        }
+
+        if (jsonObject != null) {
+            Message msg = new Message();
+            msg.what = MODE_BASIC;
+            msg.obj = true;
+            LoginActivity.loginHandle.sendMessage(msg);
+        }
+    }
+
+    private void oauthMode(OkHttpClient client) {
+        Request request = new Request.Builder()
+                .url(String.format("%s/%s", ConstManager.URL_BASE, "login/oauth"))
+                .get()
+                .build();
+
+        String url = null;
+
+        try {
+            Response response = client.newCall(request).execute();
+            url = response.body().string();
+        } catch (IOException ex) {
+            Log.d(ConstManager.TAG, "\n\nDeu na conexão");
+            ex.printStackTrace();
+            failLogin();
+        }
+
+        if (url != null) {
+            Message msg = new Message();
+            msg.what = MODE_OAUTH;
+            msg.obj = url;
+            LoginActivity.loginHandle.sendMessage(msg);
+        }
+    }
+
+    private void authorizationMode(OkHttpClient client, RequestBody requestBody) {
+        Request request = new Request.Builder()
+                .url(String.format("%s/%s", ConstManager.URL_BASE, "login/authorizate"))
+                .post(requestBody)
+                .build();
+
+        JSONObject jsonObject = null;
+
+        try {
+            Response response = client.newCall(request).execute();
+            jsonObject = new JSONObject(response.body().string());
+        } catch (IOException ex) {
+            Log.d(ConstManager.TAG, "\n\nDeu na conexão");
+            ex.printStackTrace();
+            failLogin();
+        } catch (JSONException ex) {
+            Log.d(ConstManager.TAG, "\n\nDeu ruim no JSON");
+            ex.printStackTrace();
+            failLogin();
+        }
+
+        if (jsonObject != null) {
+            Message msg = new Message();
+            msg.what = MODE_AUTHORIZATE;
+            msg.obj = true;
+            LoginActivity.loginHandle.sendMessage(msg);
+        }
+    }
+
     private void failLogin() {
         Message msg = new Message();
+        msg.what = 0;
         msg.obj = false;
         LoginActivity.loginHandle.sendMessage(msg);
     }
