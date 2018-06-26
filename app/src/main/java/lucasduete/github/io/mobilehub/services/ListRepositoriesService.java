aa9a971cc17c1c32d64f31fdb19552eb69514e20
Lucas Duete <lm.duete@gmail.com>
2018-06-25 23:21:21 -0300

    Implemetando ListRepositoriesService.

---
+++ app/src/main/java/lucasduete/github/io/mobilehub/services/ListRepositoriesService.java
@@ -0,0 +1,71 @@
+package lucasduete.github.io.mobilehub.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import lucasduete.github.io.mobilehub.ListRepositoriesActivity;
+import lucasduete.github.io.mobilehub.utils.ConstManager;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ListRepositoriesService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        new Thread(() -> {
+            Log.d(ConstManager.TAG, "Chegou no Service");
+
+            String token = getSharedPreferences(ConstManager.PREFS_NAME, MODE_PRIVATE)
+                    .getString("token", null);
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(String.format("%s/repositories/stars", ConstManager.TAG))
+                    .header("Authorization", String.format("Bearer %s", token))
+                    .get()
+                    .build();
+
+            JSONArray jsonArray = null;
+
+            try {
+                Response response = client.newCall(request).execute();
+                jsonArray = new JSONArray(response.body().string());
+                Log.d(ConstManager.TAG, "\n\nObjeto :\n" + jsonArray.toString());
+
+            } catch (IOException ex) {
+                Log.d(ConstManager.TAG, "\n\nDeu pau na conexão");
+                ex.printStackTrace();
+            } catch (JSONException ex) {
+                Log.d(ConstManager.TAG, "\n\nDeu pau na Conversão de JSON");
+                ex.printStackTrace();
+            }
+
+            if (jsonArray != null) {
+                Log.d(ConstManager.TAG, jsonArray.toString());
+                Message msg = new Message();
+                msg.obj = jsonArray;
+                ListRepositoriesActivity.listRepositoriesHander.sendMessage(msg);
+            }
+
+        }).start();
+
+        return Service.START_NOT_STICKY;
+    }
+}
