386dc5756b29f38e630fc8f05165da39c93323a1
Lucas Duete <lm.duete@gmail.com>
2018-07-31 11:15:12 -0300

    Tranformando SearchService em AsyncTask.

--- app/src/main/java/lucasduete/github/io/mobilehub/SearchActivity.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/SearchActivity.java
@@ -1,10 +1,8 @@
 package lucasduete.github.io.mobilehub;
 
 import android.content.Context;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.view.GravityCompat;
@@ -19,26 +17,26 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ListView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import lucasduete.github.io.mobilehub.adapters.RepositoryAdapter;
 import lucasduete.github.io.mobilehub.manager.MenuManage;
 import lucasduete.github.io.mobilehub.models.Repository;
-import lucasduete.github.io.mobilehub.services.SearchService;
 import lucasduete.github.io.mobilehub.utils.ConstManager;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SearchActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
-    public static SearchHandle seachHandle;
-
     private ArrayList<Repository> repositories = new ArrayList<>();
     private RepositoryAdapter adapter;
     private Context context;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,31 +44,27 @@ public class SearchActivity extends AppCompatActivity
         setContentView(R.layout.activity_search);
 
         this.context = this;
-        seachHandle = new SearchHandle();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         atualizarListView();
 
-        TextInputEditText inputText = (TextInputEditText) findViewById(R.id.inputSearch);
+        TextInputEditText inputText = findViewById(R.id.inputSearch);
         inputText.setOnEditorActionListener((view, actionId, event) -> {
-            Log.d(ConstManager.TAG, "ENTROU");
-            if (actionId == EditorInfo.IME_ACTION_SEND) {
-                Log.d(ConstManager.TAG, "\n\nENTROUUUU NO IFFFF");
-                Intent intent = new Intent(context, SearchService.class);
-                intent.putExtra("keyword", inputText.getText().toString());
-                startService(intent);
-            }
+
+            if (actionId == EditorInfo.IME_ACTION_SEND)
+                new SearchTask().execute(inputText.getText().toString());
+
             return true;
         });
 
@@ -88,19 +82,14 @@ public class SearchActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -121,16 +110,52 @@ public class SearchActivity extends AppCompatActivity
         return true;
     }
 
-    public class SearchHandle extends Handler {
-        public SearchHandle() {
+    private void atualizarListView() {
+        ListView listView = (ListView) findViewById(R.id.listViewSearch);
 
+        adapter = new RepositoryAdapter(
+                repositories,
+                this
+        );
+
+        listView.setAdapter(adapter);
+    }
+
+    private class SearchTask extends AsyncTask<String, Integer, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(String... strings) {
+            Log.d(ConstManager.TAG, "Chegou na SearchTask");
+
+            String keyword = strings[0];
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(String.format("%s/repositories/search?keyword=%s", ConstManager.URL_BASE, keyword))
+                    .get()
+                    .build();
+
+            JSONArray jsonArray = null;
+
+            try {
+                Response response = client.newCall(request).execute();
+                jsonArray = new JSONArray(response.body().string());
+                Log.d(ConstManager.TAG, "\n\nObjeto: \n" + jsonArray.toString());
+
+            } catch (IOException ex) {
+                Log.d(ConstManager.TAG, "\n\nDeu pau na conexão");
+                ex.printStackTrace();
+            } catch (JSONException ex) {
+                Log.d(ConstManager.TAG, "\n\nDeu pau na Conversão de JSON");
+                ex.printStackTrace();
+            }
+
+            return jsonArray;
         }
 
-        //TODO centralizar este codigo
         @Override
-        public void handleMessage(Message msg) {
+        protected void onPostExecute(JSONArray jsonArray) {
             ArrayList<Repository> repositoriesTemp = new ArrayList<>();
-            JSONArray jsonArray = (JSONArray) msg.obj;
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 try {
@@ -154,17 +179,6 @@ public class SearchActivity extends AppCompatActivity
             repositories.addAll(repositoriesTemp);
             atualizarListView();
         }
-    }
-
-    private void atualizarListView() {
-        ListView listView = (ListView) findViewById(R.id.listViewSearch);
-
-        adapter = new RepositoryAdapter(
-                repositories,
-                this
-        );
-
-        listView.setAdapter(adapter);
     }
 
 }
