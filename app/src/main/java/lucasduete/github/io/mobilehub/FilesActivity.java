68af8f2cd26ffe3753c2a14a2c9c60e64b5b36f2
Lucas Duete <lm.duete@gmail.com>
2018-08-01 15:47:55 -0300

    Corrigindo Erro no WebView de FilesActivity, Adicionando Thread para capturar informações do repositório.

--- app/src/main/java/lucasduete/github/io/mobilehub/FilesActivity.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/FilesActivity.java
@@ -2,6 +2,7 @@ package lucasduete.github.io.mobilehub;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,17 +10,29 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Downloader;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 import lucasduete.github.io.mobilehub.manager.MenuManage;
+import lucasduete.github.io.mobilehub.models.Repository;
 import lucasduete.github.io.mobilehub.services.DownloadService;
+import lucasduete.github.io.mobilehub.utils.ConstManager;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class FilesActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -91,8 +104,10 @@ public class FilesActivity extends AppCompatActivity
     protected void onStart() {
         super.onStart();
 
+        new RepositoryTask().execute(repoName);
+
         WebView webView = findViewById(R.id.webViewFiles);
-        webView.loadUrl(String.format("https://github.com/%s/%s", repoOwner, repoName));
+        webView.loadUrl(String.format("https://github.com/%s?files=1", repoName));
     }
 
     @Override
@@ -131,5 +146,74 @@ public class FilesActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class RepositoryTask extends AsyncTask<String, Integer, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            Log.d(ConstManager.TAG, "Chegou na Task de Repository");
+
+            String repoName = strings[0];
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(String.format("%s/repositories/%s", ConstManager.URL_BASE, repoName))
+                    .get()
+                    .build();
+
+            JSONObject jsonObject = null;
+
+            try {
+                Response response = client.newCall(request).execute();
+                jsonObject = new JSONObject(response.body().string());
+                Log.d(ConstManager.TAG, "\nObjeto: " + jsonObject.toString());
+
+            } catch (IOException ex) {
+                Log.d(ConstManager.TAG, "Deu pau na conxão");
+                ex.printStackTrace();
+            } catch (JSONException ex) {
+                Log.d(ConstManager.TAG, "Deu pau na conversão de Json");
+                ex.printStackTrace();
+            }
+
+            return jsonObject;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            TextView textView;
+            Repository repository = new Repository();
+
+            try {
+                repository.setStars(jsonObject.getInt("stars"));
+                repository.setForks(jsonObject.getInt("forks"));
+                repository.setNome(jsonObject.getString("nome"));
+                repository.setFoto(jsonObject.getString("fotoAutor"));
+                repository.setDescricao(jsonObject.getString("descricao"));
+                repository.setNomeAutor(jsonObject.getString("nomeAutor"));
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                Toast.makeText(context, "Erro ao Recupear as Informações.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            textView = findViewById(R.id.textViewRepositoryName);
+            textView.setText(repository.getNome());
+
+            textView = findViewById(R.id.textViewRepositoryDescription);
+            if (repository.getDescricao().equalsIgnoreCase("null"))
+                textView.setText("(Nenhuma Descrição Fornecida)");
+            else
+                textView.setText(repository.getDescricao());
+
+            textView = findViewById(R.id.textViewNumberStras);
+            textView.setText(String.valueOf(repository.getStars()));
+
+            textView = findViewById(R.id.textViewNumberForks);
+            textView.setText(String.valueOf(repository.getForks()));
+
+        }
     }
 }
