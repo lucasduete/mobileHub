f03619d8041ab0da2125da90f8d7559ad12922ec
Lucas Duete <lm.duete@gmail.com>
2018-08-01 09:36:15 -0300

    Refatorando FilesActivity para utilizar WebView para listagem dos arquivos ao inves de ListView.

--- app/src/main/java/lucasduete/github/io/mobilehub/FilesActivity.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/FilesActivity.java
@@ -12,21 +12,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.squareup.picasso.Downloader;
 
 import lucasduete.github.io.mobilehub.manager.MenuManage;
+import lucasduete.github.io.mobilehub.services.DownloadService;
 
 public class FilesActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ArrayAdapter<String> adapter;
+    String repo = null;
+    String owner = null;
     private Context context;
+//    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,54 +35,69 @@ public class FilesActivity extends AppCompatActivity
         setContentView(R.layout.activity_files);
 
         this.context = this;
+        this.repo = getIntent().getStringExtra("repo");
+        this.owner = getIntent().getStringExtra("owner");
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Button issuesButton = (Button) findViewById(R.id.buttonIssues);
+        Button issuesButton = findViewById(R.id.buttonIssues);
         issuesButton.setOnClickListener((View view) -> {
             Intent intent = new Intent(context, ListIssueActivity.class);
             startActivity(intent);
         });
 
-        Button repositoryButton = (Button) findViewById(R.id.buttonRepository);
+        Button repositoryButton = findViewById(R.id.buttonRepository);
         repositoryButton.setOnClickListener(view -> {
             Intent intent = new Intent(context, RepositoryActivity.class);
             finish();
             startActivity(intent);
         });
 
-        Button downloadButton = (Button) findViewById(R.id.buttonDownload);
-        downloadButton.setOnClickListener(view -> Toast
-                .makeText(context, "TU BAIXOUUU CARA e.e <3 :3", Toast.LENGTH_SHORT)
-                .show());
+        Button downloadButton = findViewById(R.id.buttonDownload);
+        downloadButton.setOnClickListener(view -> {
+            Intent intent = new Intent(context, DownloadService.class);
 
-        ListView listView = (ListView) findViewById(R.id.listViewFiles);
-        ArrayList<String> arrayFiles = new ArrayList<>();
-        arrayFiles.addAll(Arrays.asList(getResources().getStringArray(R.array.static_list_files)));
+            intent.putExtra("repo", this.repo);
+            intent.putExtra("owner", this.owner);
 
-        adapter = new ArrayAdapter<String>(
-                FilesActivity.this,
-                android.R.layout.simple_list_item_1,
-                arrayFiles
-        );
+            startService(intent);
+        });
 
-        listView.setAdapter(adapter);
+//        ListView listView = (ListView) findViewById(R.id.listViewFiles);
+//        ArrayList<String> arrayFiles = new ArrayList<>();
+//        arrayFiles.addAll(Arrays.asList(getResources().getStringArray(R.array.static_list_files)));
+//
+//        adapter = new ArrayAdapter<String>(
+//                FilesActivity.this,
+//                android.R.layout.simple_list_item_1,
+//                arrayFiles
+//        );
+//
+//        listView.setAdapter(adapter);
 
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        WebView webView = findViewById(R.id.webViewFiles);
+        webView.loadUrl(String.format("https://github.com/%s/%s", owner, repo));
+    }
+
+    @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -91,22 +107,15 @@ public class FilesActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        if (id == R.id.action_settings) return true;
 
         return super.onOptionsItemSelected(item);
     }
@@ -119,7 +128,7 @@ public class FilesActivity extends AppCompatActivity
                 MenuManage.getActivity(this, item)
         );
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
