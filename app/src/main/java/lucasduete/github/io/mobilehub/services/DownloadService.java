07b52ccb07327940d611f07aa8b6c8b4689829bc
Lucas Duete <lm.duete@gmail.com>
2018-08-01 15:38:28 -0300

    Corrigindo erros e melhorando DownloadService

--- app/src/main/java/lucasduete/github/io/mobilehub/services/DownloadService.java
+++ app/src/main/java/lucasduete/github/io/mobilehub/services/DownloadService.java
@@ -1,11 +1,13 @@
 package lucasduete.github.io.mobilehub.services;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
@@ -13,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import lucasduete.github.io.mobilehub.broadcastReceivers.BatteryStatusReceiver;
+import lucasduete.github.io.mobilehub.utils.ConstManager;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -23,6 +26,8 @@ public class DownloadService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        final Toast toast = Toast.makeText(this, "Error", Toast.LENGTH_SHORT);
+
         final BatteryStatusReceiver batteryReceiver = new BatteryStatusReceiver(this.downloadThread);
 
         this.downloadThread = new Thread(() -> {
@@ -31,31 +36,47 @@ public class DownloadService extends Service {
 
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
-                    .url(String.format("https://codeload.github.com/%s/%s/zip/master", repoOwner, repoName))
+                    .url(String.format("https://codeload.github.com/%s/zip/master", repoName))
                     .get()
                     .build();
 
             String state = Environment.getExternalStorageState();
             if (!Environment.MEDIA_MOUNTED.equals(state)) {
-                Toast.makeText(this, "Impossível Salvar Arquivo", Toast.LENGTH_SHORT).show();
+                toast.setText("Impossível Salvar Arquivo");
+                toast.show();
                 return;
             }
 
             try {
+                toast.setText("Download Iniciado");
+                toast.show();
+
                 Response response = client.newCall(request).execute();
 
-                File file = new File(
-                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                        String.format("%s/%s-master.zip", repoOwner, repoName));
-                FileOutputStream outputStream = new FileOutputStream(file);
+                String fileName = repoName.replaceFirst("/", "_").concat("-master.zip");
+                String filePath = String.format("%s/%s", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+
+                FileOutputStream outputStream = new FileOutputStream(filePath);
 
                 outputStream.write(response.body().bytes());
 
                 outputStream.flush();
                 outputStream.close();
+
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                toast.setText("Download Finalizado");
+                toast.show();
             } catch (IOException ex) {
-                Toast.makeText(this, "Não foi possível baixar o repositório", Toast.LENGTH_SHORT).show();
+
                 ex.printStackTrace();
+
+                toast.setText("Não foi possível baixar o repositório");
+                toast.show();;
             }
 
             unregisterReceiver(batteryReceiver);
